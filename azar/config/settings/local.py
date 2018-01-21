8593cd06612d88da788773b9f5b710ffa7c811b7
Mark Kim <viking617617@gmail.com>
2018-01-21 16:00:40 +0900

    Settings: seperated settings into local, production version

---
+++ azar/config/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+config_secret_local = json.loads(open(CONFIG_SECRET_LOCAL_FILE).read())
+
+# Database
+DATABASES = config_secret_local['django']['databases']
+
+# Local Installed Apps
+INSTALLED_APPS += [
+    'django_extensions'
+]
+
+# Debug
+DEBUG = True
