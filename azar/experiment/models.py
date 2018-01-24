8615da4894d33956a2c06a2467561fd23d8d5f63
Mark Kim <viking617617@gmail.com>
2018-01-24 14:17:49 +0900

    Settings: Docker - 'eb deploy' 작동 확인

--- azar/experiment/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from config.storage_backends.custom_storage_backend import PrivateMediaStorage
-
-
-class Document(models.Model):
-    image = models.ImageField(storage=PrivateMediaStorage())
